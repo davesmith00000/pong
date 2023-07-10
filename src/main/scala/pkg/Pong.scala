08a191384fb8e2b5f662487f623a5585bfcacb32
Dave Smith <davesmith.00000@gmail.com>
2023-07-10 20:03:52 +0100

    Refactoring

--- src/main/scala/pkg/Pong.scala
+++ src/main/scala/pkg/Pong.scala
@@ -7,7 +7,7 @@ import indigo.scenes.*
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("IndigoGame")
-object Pong extends IndigoSandbox[Unit, Model]:
+object Pong extends IndigoSandbox[Size, Model]:
 
   val config: GameConfig =
     GameConfig.default
@@ -21,59 +21,14 @@ object Pong extends IndigoSandbox[Unit, Model]:
   def setup(
       assetCollection: AssetCollection,
       dice: Dice
-  ): Outcome[Startup[Unit]] =
-    Outcome(Startup.Success(()))
+  ): Outcome[Startup[Size]] =
+    Outcome(Startup.Success(config.viewport.size))
 
-  val paddle            = Rectangle(10, 50)
-  val ballStartPosition = Vertex(270, 195)
-
-  def initialModel(startupData: Unit): Outcome[Model] =
-    val a = paddle.withPosition(30, 50)
-    val b = paddle.withPosition(510, 50)
-
-    Outcome(
-      Model(
-        scoreA = 0,
-        scoreB = 0,
-        paddleA = a,
-        paddleB = b,
-        world = World
-          .empty[String]
-          .withColliders(
-            Collider
-              .Box("left goal", BoundingBox(-50, -100, 50, 600))
-              .makeStatic,
-            Collider
-              .Box("right goal", BoundingBox(550, -100, 50, 600))
-              .makeStatic,
-            Collider.Box("top wall", BoundingBox(10, 10, 530, 10)).makeStatic,
-            Collider
-              .Box("bottom wall", BoundingBox(10, 380, 530, 10))
-              .makeStatic,
-            Collider
-              .Circle("ball", BoundingCircle(ballStartPosition, 10))
-              .withVelocity(100, 100)
-              .onCollision {
-                case c if c.tag.startsWith("paddle") =>
-                  Batch(GameEvent.SpeedUp)
-
-                case c if c.tag == "left goal" =>
-                  Batch(GameEvent.ScoreA, GameEvent.ResetBall)
-
-                case c if c.tag == "right goal" =>
-                  Batch(GameEvent.ScoreB, GameEvent.ResetBall)
-
-                case _ =>
-                  Batch()
-              },
-            Collider.Box("paddle a", a.toBoundingBox).makeStatic,
-            Collider.Box("paddle b", b.toBoundingBox).makeStatic
-          )
-      )
-    )
+  def initialModel(viewportSize: Size): Outcome[Model] =
+    Outcome(Model.initial(viewportSize))
 
   def updateModel(
-      context: FrameContext[Unit],
+      context: FrameContext[Size],
       model: Model
   ): GlobalEvent => Outcome[Model] =
     case FrameTick =>
@@ -83,8 +38,8 @@ object Pong extends IndigoSandbox[Unit, Model]:
         Model.movePaddle(model.paddleB, context.inputState.mouse.position.y)
 
       model.world
-        .modifyByTag("paddle a")(_.moveTo(model.paddleA.position.toVertex))
-        .modifyByTag("paddle b")(_.moveTo(model.paddleB.position.toVertex))
+        .modifyByTag(Tags.PaddleA)(_.moveTo(model.paddleA.position.toVertex))
+        .modifyByTag(Tags.PaddleB)(_.moveTo(model.paddleB.position.toVertex))
         .update(context.delta)
         .map { updatedWorld =>
           model.copy(
@@ -96,7 +51,9 @@ object Pong extends IndigoSandbox[Unit, Model]:
 
     case GameEvent.SpeedUp =>
       val nextWorld =
-        model.world.modifyByTag("ball")(b => b.withVelocity(b.velocity * 1.5))
+        model.world.modifyByTag(Tags.Ball)(b =>
+          b.withVelocity(b.velocity * Model.velocityMultiplier)
+        )
 
       Outcome(model.copy(world = nextWorld))
 
@@ -116,8 +73,10 @@ object Pong extends IndigoSandbox[Unit, Model]:
 
       val nextWorld =
         model.world
-          .modifyByTag("ball")(
-            _.moveTo(ballStartPosition).withVelocity(giveVec * 100)
+          .modifyByTag(Tags.Ball)(
+            _.moveTo(model.ballStartPosition).withVelocity(
+              giveVec * Model.velocityAfterReset
+            )
           )
 
       Outcome(model.copy(world = nextWorld))
@@ -126,7 +85,7 @@ object Pong extends IndigoSandbox[Unit, Model]:
       Outcome(model)
 
   def present(
-      context: FrameContext[Unit],
+      context: FrameContext[Size],
       model: Model
   ): Outcome[SceneUpdateFragment] =
     Outcome(
@@ -143,7 +102,9 @@ object Pong extends IndigoSandbox[Unit, Model]:
             .moveTo(550 / 2, 30)
         ),
         Layer(
-          model.world.presentNot(_.tag.contains("goal")) {
+          model.world.presentNot(c =>
+            c.tag == Tags.LeftGoal || c.tag == Tags.RightGoal
+          ) {
             case Collider.Circle(_, bounds, _, _, _, _, _, _, _) =>
               Shape.Circle(
                 bounds.position.toPoint,
@@ -162,14 +123,18 @@ object Pong extends IndigoSandbox[Unit, Model]:
     )
 
 final case class Model(
+    ballStartPosition: Vertex,
     scoreA: Int,
     scoreB: Int,
     paddleA: Rectangle,
     paddleB: Rectangle,
-    world: World[String]
+    world: World[Tags]
 )
 
 object Model:
+
+  val velocityAfterReset         = Vector2(100, 100)
+  val velocityMultiplier: Double = 1.5
 
   def movePaddle(paddle: Rectangle, mouseY: Int): Rectangle =
     val top    = 20
@@ -183,5 +148,79 @@ object Model:
 
     paddle.moveTo(paddle.x, nextY)
 
+  def initial(viewportSize: Size): Model =
+    val paddle = Rectangle(10, 50)
+    val gap    = 30
+    val a      = paddle.withPosition(gap, viewportSize.height / 2)
+    val b = paddle.withPosition(
+      viewportSize.width - gap - paddle.width,
+      viewportSize.height / 2
+    )
+    val start = (viewportSize / 2).toVertex
+
+    Model(
+      ballStartPosition = start,
+      scoreA = 0,
+      scoreB = 0,
+      paddleA = a,
+      paddleB = b,
+      world = World
+        .empty[Tags]
+        .withColliders(
+          Collider
+            .Box(
+              Tags.LeftGoal,
+              BoundingBox(-50, -100, 50, viewportSize.height + 200)
+            )
+            .makeStatic,
+          Collider
+            .Box(
+              Tags.RightGoal,
+              BoundingBox(
+                viewportSize.width + 50,
+                -100,
+                50,
+                viewportSize.height + 200
+              )
+            )
+            .makeStatic,
+          Collider
+            .Box(Tags.Wall, BoundingBox(10, 10, viewportSize.width - 20, 10))
+            .makeStatic,
+          Collider
+            .Box(
+              Tags.Wall,
+              BoundingBox(
+                10,
+                viewportSize.height - 20,
+                viewportSize.width - 20,
+                10
+              )
+            )
+            .makeStatic,
+          Collider
+            .Circle(Tags.Ball, BoundingCircle(start, 10))
+            .withVelocity(velocityAfterReset)
+            .onCollision {
+              case c if c.tag == Tags.PaddleA || c.tag == Tags.PaddleB =>
+                Batch(GameEvent.SpeedUp)
+
+              case c if c.tag == Tags.LeftGoal =>
+                Batch(GameEvent.ScoreB, GameEvent.ResetBall)
+
+              case c if c.tag == Tags.RightGoal =>
+                Batch(GameEvent.ScoreA, GameEvent.ResetBall)
+
+              case _ =>
+                Batch()
+            },
+          Collider.Box(Tags.PaddleA, a.toBoundingBox).makeStatic,
+          Collider.Box(Tags.PaddleB, b.toBoundingBox).makeStatic
+        )
+    )
+
 enum GameEvent extends GlobalEvent:
   case SpeedUp, ScoreA, ScoreB, ResetBall
+
+enum Tags:
+  case Ball, PaddleA, PaddleB, Wall, LeftGoal, RightGoal
